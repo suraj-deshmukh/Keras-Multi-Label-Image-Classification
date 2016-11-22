e89657e7654eb95fc62e3f96e3521bda7505a8d0
Suraj Deshmukh <suraj-deshmukh@users.noreply.github.com>
2016-11-22 12:28:19 +0530

    Add files via upload

---
+++ generate-data.py
@@ -0,0 +1,33 @@
+import numpy as np
+import h5py
+import cv2
+import scipy.io as sio
+import os
+from skimage import io
+from skimage.transform import resize
+
+target_path = "/home/suraj/Documents/datasets/miml-image-data/miml data.mat"
+image_path = "/home/suraj/Documents/datasets/miml-image-data/original"
+
+y = sio.loadmat(target_path)
+y = y['targets']
+y = y.transpose()
+y = np.array([[elem if elem == 1 else 0 for elem in row]for row in y])
+x = []
+
+for i in range(1,2001):
+    print "reading image:"+str(i) + ".jpg"
+    img = image_path + "/" + str(i) + ".jpg"
+    img = cv2.imread(img)
+    img = cv2.resize(img,(100,100))
+    img = img.transpose((2,0,1))
+    # img = io.imread(img)
+    # img = resize(img,(100,100))
+    # img = img.transpose()
+    x.append(img)
+    
+x = np.array(x)
+f = h5py.File("dataset.h5")
+f['x'] = x
+f['y'] = y
+f.close()
---
+++ getdata.py
@@ -0,0 +1,11 @@
+import h5py
+from sklearn.cross_validation import train_test_split
+
+def load():
+    f = h5py.File("dataset.h5")
+    x = f['x'].value
+    y = f['y'].value
+    f.close()
+    x_train , x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=100)
+    return x_train, x_test, y_train, y_test
+    
