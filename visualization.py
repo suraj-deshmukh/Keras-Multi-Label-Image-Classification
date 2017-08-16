b54c8c6c021d1ec43d286af1a36543d164914a04
Suraj Deshmukh <suraj-deshmukh@users.noreply.github.com>
2017-08-16 10:22:11 +0530

    Add files via upload
    visualization of filters

---
+++ visualization.py
@@ -0,0 +1,40 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras import backend as K
+from quiver_engine import server
+K.set_image_dim_ordering('th')
+
+
+model = Sequential()
+model.add(Convolution2D(32, kernel_size=(3, 3),padding='same',input_shape=(3 , 100, 100)))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(64,(3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(5))
+model.add(Activation('sigmoid'))
+
+
+# let's train the model using SGD + momentum (how original).
+sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=False)
+
+model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.load_weights("weights.hdf5")
+
+server.launch(model,input_folder='./',temp_folder='./filters')
